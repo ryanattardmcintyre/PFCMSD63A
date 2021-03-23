1b0d8686dbdaaea8ed70c528d943305cac3164c1
ryan.attard <ryan.attard@mcast.edu.mt>
2021-03-23 13:22:43 +0100

    pub sub part 1

---
+++ PFCMSD63A/WebApplication1/DataAccess/Interfaces/IPubSubRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DataAccess.Repositories;
+using WebApplication1.Models.Domain;
+
+namespace WebApplication1.DataAccess.Interfaces
+{
+   public  interface IPubSubRepository
+    {
+        void PublishMessage(Blog b, string email, string category);
+
+        string PullMessage(Category cat);
+    }
+}
---
+++ PFCMSD63A/WebApplication1/DataAccess/Repositories/PubSubRepository.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DataAccess.Interfaces;
+using WebApplication1.Models.Domain;
+using Google.Cloud.PubSub.V1;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Google.Protobuf;
+using Grpc.Core;
+
+namespace WebApplication1.DataAccess.Repositories
+{
+   public enum Category { economy, business, luxury}
+
+
+    public class PubSubRepository : IPubSubRepository
+    {
+        string projectId;
+
+        public PubSubRepository(IConfiguration config)
+        {
+            projectId = config.GetSection("AppSettings").GetSection("ProjectId").Value;
+        }
+
+
+        public void PublishMessage(Blog b, string email, string classType)
+        {
+            TopicName topicName = TopicName.FromProjectTopic(projectId, "pfc2021topicra");
+
+            Task<PublisherClient> t = PublisherClient.CreateAsync(topicName);
+            t.Wait();
+            PublisherClient publisher = t.Result  ;
+
+            var myOnTheFlyObject = new { Email = email, Blog = b }; //anonymous object
+            string myOnTheflyObject_serialized = JsonConvert.SerializeObject(myOnTheFlyObject);
+
+            var pubsubMessage = new PubsubMessage
+            {
+                // The data is any arbitrary ByteString. Here, we're using text.
+                Data = ByteString.CopyFromUtf8(myOnTheflyObject_serialized),                //Ctrl + .
+                // The attributes provide metadata in a string-to-string dictionary.
+                Attributes =
+                        {
+                            { "category", classType }
+                        }
+            };
+
+            Task<string> t2 = publisher.PublishAsync(pubsubMessage);
+            t2.Wait();
+            string message = t2.Result ; //reference no/ id //log id date and time it was published
+            
+        }
+
+        public string PullMessage(Category cat)
+        {
+            //string subscriptionid;
+            //switch(cat)
+            //{
+            //    case Category.business:
+            //        subscriptionid = "pfc2021subscriptionra3";
+            //        break;
+
+            //    case Category.economy:
+            //        subscriptionid = "pfc2021subscriptionra4";
+            //        break;
+            //}
+
+
+            SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(projectId, "pfc2021subscriptionra3");
+            SubscriberServiceApiClient subscriberClient = SubscriberServiceApiClient.Create();
+            int messageCount = 0;string text ="";
+            try
+            {
+                // Pull messages from server,
+                // allowing an immediate response if there are no messages.
+                PullResponse response = subscriberClient.Pull(subscriptionName, returnImmediately: true, maxMessages: 1);
+                // Print out each received message.
+                
+                var msg = response.ReceivedMessages.FirstOrDefault();
+                if(msg != null)
+                {
+                    text = msg.Message.Data.ToStringUtf8();
+                }
+
+                //  subscriberClient.Acknowledge(subscriptionName, response.ReceivedMessages.Select(msg => msg.AckId)); //this acknowledges more than 1 message 
+                subscriberClient.Acknowledge(subscriptionName, new List<string>() {msg.AckId });
+            }
+            catch (RpcException ex) when (ex.Status.StatusCode == StatusCode.Unavailable)
+            {
+                // UNAVAILABLE due to too many concurrent pull requests pending for the given subscription.
+
+                //log when there is an error
+            }
+            return text;
+
+        }
+    }
+}
